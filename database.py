4f361fb431b1b685ac5fad49fe5e7089f866b460
learning53 <144656227+learning53@users.noreply.github.com>
2024-04-19 14:51:50 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'mysql+pymysql://root:Sundar@localhost/placement'
+# Read database URL from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Ensure DATABASE_URL is set
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+# Create the SQLAlchemy engine
 db_engine = create_engine(DATABASE_URL)
+
+# Create a sessionmaker
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
 
-base=declarative_base()
+# Create a base class for declarative models
+Base = declarative_base()
