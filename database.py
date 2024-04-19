5a89fa8cdb61fab2af77c73256275df25169f0c4
learning53 <144656227+learning53@users.noreply.github.com>
2024-04-19 17:46:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Define the database URL
-DATABASE_URL = 'postgres://root:5HveBxrbtFCFfmpmn2SFdeGrxjv1LKCS@dpg-coh4htuv3ddc73fh2jvg-a.oregon-postgres.render.com/nec_placement'
+DATABASE_URL = 'postgresql://root:5HveBxrbtFCFfmpmn2SFdeGrxjv1LKCS@dpg-coh4htuv3ddc73fh2jvg-a.oregon-postgres.render.com/nec_placement'
 
 # Create the database engine with the specific dialect
 db_engine = create_engine(DATABASE_URL, dialect='postgresql')
