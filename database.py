33a6886dfd00d8bb7c24a4feabd3982e32a5fedd
learning53 <144656227+learning53@users.noreply.github.com>
2024-04-20 21:34:52 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import mysql as db
 
 # Define the database URL
 DATABASE_URL = 'postgresql://root:5HveBxrbtFCFfmpmn2SFdeGrxjv1LKCS@dpg-coh4htuv3ddc73fh2jvg-a.oregon-postgres.render.com/nec_placement'
 
 # Create the database engine with the specific dialect
-db_engine = db.create_engine(
+db_engine = create_engine(
     DATABASE_URL,
     pool_size=5,  # Set the pool size to 5 connections
     max_overflow=10,  # Allow up to 10 additional connections to be created temporarily
